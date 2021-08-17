259a3bd5a2fc336e47bf593d2bc027aee33f6765
ahadsenja <hamdanilm96@gmail.com>
2021-08-17 17:16:50 +0800

    feat: create auth interceptor

---
+++ src/app/core/interceptor/auth.interceptor.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { TokenStorageService } from '../auth/token-storage.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private token: TokenStorageService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const authToken =JSON.parse(this.token.getToken());
+    if (authToken !== null) {
+      const authReq = request.clone({
+        headers: request.headers.set(
+          'Authorization', `Bearer ${authToken.token}`
+        )
+      });
+      return next.handle(authReq);
+    } else {
+      return next.handle(request);
+    }
+  }
+}
