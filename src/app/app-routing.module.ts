83e32db9734c83de48ec443771273f3de43ac5e1
ahadsenja <hamdanilm96@gmail.com>
2021-08-17 17:27:08 +0800

    refactor: add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthGuard } from './core/guard/auth.guard';
+
+import { LoginComponent } from './features/pages/login/login.component';
+import { RegisterComponent } from './features/pages/register/register.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { JurnalComponent } from './features/jurnal/jurnal.component';
+import { TransactionComponent } from './features/transaction/transaction.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'jurnal', component: JurnalComponent, canActivate: [AuthGuard] },
+  { path: 'transaction', component: TransactionComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
