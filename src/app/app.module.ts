93616dd4df2f2fa37e8190ecec17822daafbabaa
ahadsenja <hamdanilm96@gmail.com>
2021-08-17 17:30:47 +0800

    chore: update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CoreModule } from './core/core.module';
+import { FeaturesModule } from './features/features.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    FeaturesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
