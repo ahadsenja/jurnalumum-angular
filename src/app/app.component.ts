8b2fb94cbf2244402ddd9ab0eddc7e00215435bd
ahadsenja <hamdanilm96@gmail.com>
2021-08-17 17:30:03 +0800

    refactor: adding isLoggedIn condition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { TokenStorageService } from './core/auth/token-storage.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jurnalumum-angular';
+
+  isLoggedIn = false;
+
+  constructor(private token: TokenStorageService) {}
+
+  ngOnInit() {
+    if (this.token.getToken()) {
+      this.isLoggedIn = true;
+    }
+  }
 }
