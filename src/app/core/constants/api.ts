52790eabb7ed8f1349da2f17174637fb747ed3df
ahadsenja <hamdanilm96@gmail.com>
2021-08-17 18:30:07 +0800

    feat: add api constant url

---
+++ src/app/core/constants/api.ts
@@ -0,0 +1,6 @@
+export class Api {
+ public static LOGIN_URL = 'http://localhost:5000/auth/login';
+ public static REGISTER_URL = 'http://localhost:5000/auth/register';
+ public static JURNAL_URL = 'http://localhost:5000/jurnals';
+ public static TRANSACTION_URL = 'http://localhost:5000/transactions';
+}
