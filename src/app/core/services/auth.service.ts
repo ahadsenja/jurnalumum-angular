7defa87b3ac4a9ba47d9ddf9fd5a18b525a0bd2b
ahadsenja <hamdanilm96@gmail.com>
2021-08-18 23:37:53 +0800

    fix: fixing login service

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
+
+import { Api } from '../constants/api';
+import { TokenStorageService } from '../auth/token-storage.service';
+import { User } from 'src/app/shared/models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient, private token: TokenStorageService) { }
+
+  login(email: string, password: string): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.post(Api.LOGIN_URL, {email, password}, httpOptions).pipe(map(token => {
+      this.token.saveToken(JSON.stringify(token))
+    }))
+  }
+}
