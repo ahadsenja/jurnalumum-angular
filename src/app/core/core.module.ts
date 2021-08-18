391e6419228937cc92e8da9ece42be18d9c0c229
ahadsenja <hamdanilm96@gmail.com>
2021-08-19 06:05:32 +0800

    feat: add AuthGuard and HTTP_INTERCEPTOR

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AuthGuard } from './guard/auth.guard';
+import { AuthInterceptor } from './interceptor/auth.interceptor';
+
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -10,10 +15,20 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     CommonModule
   ],
+
   exports: [
     NavbarComponent,
     SidebarComponent,
     FooterComponent
+  ],
+
+  providers: [
+    AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ]
 })
 export class CoreModule { }
