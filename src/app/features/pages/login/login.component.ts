38390bdf85e80f82ec617c0a10df78a0d6b3906f
ahadsenja <hamdanilm96@gmail.com>
2021-08-18 23:35:51 +0800

    fix: change from reactivefrom into driven form

--- src/app/features/pages/login/login.component.ts
+++ src/app/features/pages/login/login.component.ts
@@ -1,4 +1,9 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenStorageService } from 'src/app/core/auth/token-storage.service';
+
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: any = {
+    email: '',
+    password: ''
+  }
+
+  isLoggedIn = false;
+
+  constructor(
+    private authService: AuthService,
+    private token: TokenStorageService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    const { email, password } = this.form;
+
+    if (email !== '' && password !== '') {
+      this.authService.login(email, password).subscribe(data => {
+        this.isLoggedIn = true;
+        this.token.saveUser(data);
+        this.location.replaceState('/');
+        window.location.reload();
+      })
+    }
+  }
 }
