28b6777b009f2e66f889ceead25f0b49df69e979
ahadsenja <hamdanilm96@gmail.com>
2021-08-19 05:54:47 +0800

    chore: update folder name to jurnal-list, and insert it into jurnal parent folder

---
+++ src/app/features/jurnal/jurnal-list/jurnal.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Api } from 'src/app/core/constants/api';
+import { Jurnal } from 'src/app/core/models/jurnal';
+import { JurnalService } from '../../services/jurnal.service';
+
+@Component({
+  selector: 'app-jurnal',
+  templateUrl: './jurnal.component.html',
+  styleUrls: ['./jurnal.component.css']
+})
+export class JurnalComponent implements OnInit {
+
+  jurnals: Jurnal[] = [];
+
+  constructor(private jurnalService: JurnalService) { }
+
+  ngOnInit(): void {
+    this.onGetJurnals();
+  }
+
+  onGetJurnals() {
+    this.jurnalService.getAll().subscribe(jurnals => {
+      this.jurnals = jurnals;
+      console.log(jurnals);
+    })
+  }
+
+}
