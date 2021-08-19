888a6032fb33470e2bb6f37f4b3781abad4b5309
ahadsenja <hamdanilm96@gmail.com>
2021-08-19 17:30:38 +0800

    feat: add jurnal and transaction create url

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { JurnalCreateComponent } from './jurnal/jurnal-create/jurnal-create.component';
 import { JurnalComponent } from './jurnal/jurnal-list/jurnal.component';
+import { TransactionCreateComponent } from './transaction/transaction-create/transaction-create.component';
 import { TransactionComponent } from './transaction/transaction-list/transaction.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'jurnal', component: JurnalComponent },
+  { path: 'jurnal/create', component: JurnalCreateComponent },
   { path: 'transaction', component: TransactionComponent },
+  { path: 'transaction/create', component: TransactionCreateComponent },
 ];
 
 @NgModule({
