3d37eb63a1c4199e99153e587e292c25218626d9
ahadsenja <hamdanilm96@gmail.com>
2021-08-19 16:19:36 +0800

    feat: create trannsaction-create component

---
+++ src/app/features/transaction/transaction-create/transaction-create.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TransactionCreateComponent } from './transaction-create.component';
+
+describe('TransactionCreateComponent', () => {
+  let component: TransactionCreateComponent;
+  let fixture: ComponentFixture<TransactionCreateComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TransactionCreateComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TransactionCreateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
