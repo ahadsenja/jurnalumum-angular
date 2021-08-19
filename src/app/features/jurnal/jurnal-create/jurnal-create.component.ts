cd821edea3cab7d1953d0405c3d677a2672e0ef8
ahadsenja <hamdanilm96@gmail.com>
2021-08-19 17:20:20 +0800

    refactor: add jurnal form create data

--- src/app/features/jurnal/jurnal-create/jurnal-create.component.ts
+++ src/app/features/jurnal/jurnal-create/jurnal-create.component.ts
@@ -1,4 +1,10 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { Jurnal } from 'src/app/shared/models/jurnal';
+import { JurnalService } from '../../services/jurnal.service';
 
 @Component({
   selector: 'app-jurnal-create',
@@ -7,9 +13,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JurnalCreateComponent implements OnInit {
 
-  constructor() { }
+  jurnalCreateFG = new FormGroup({});
+
+  isSubmitted = false;
+
+  jurnals: Jurnal[] = [];
+
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private location: Location,
+    private jurnalService: JurnalService
+  ) { }
 
   ngOnInit(): void {
+    this.jurnalCreateFG = this.formBuilder.group({
+      name: [''],
+      description: [''],
+      userId: ['']
+    })
+  }
+
+  onCreateJurnal() {
+    let formData: any = new FormData();
+
+    formData.append('name', this.jurnalCreateFG.get('name')?.value);
+    formData.append('description', this.jurnalCreateFG.get('description')?.value);
+    formData.append('userId', this.jurnalCreateFG.get('userId')?.value);
+
+    this.jurnalService.create(formData).subscribe(res => {
+      console.log(res);
+      this.isSubmitted = true;
+    })
+    this.location.replaceState('/jurnal');
+  }
+
+  onCancel() {
+    this.router.navigate(['/jurnal']);
   }
 
 }
