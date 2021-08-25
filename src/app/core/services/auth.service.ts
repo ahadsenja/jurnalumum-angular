e82e98be1a2bcd15dfbbaf25b29991f41f0eebf4
ahadsenja <hamdanilm96@gmail.com>
2021-08-25 23:12:53 +0800

    refactor: update login service

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,17 +1,15 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators'
 
 import { Api } from '../constants/api';
-import { TokenStorageService } from '../auth/token-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient, private token: TokenStorageService) { }
+  constructor(private http: HttpClient) { }
 
   login(email: string, password: string): Observable<any> {
     const httpOptions = {
@@ -19,8 +17,6 @@ export class AuthService {
         'Content-Type': 'application/json'
       })
     }
-    return this.http.post(Api.LOGIN_URL, {email, password}, httpOptions).pipe(map(token => {
-      this.token.saveToken(JSON.stringify(token))
-    }))
+    return this.http.post(Api.LOGIN_URL, {email, password}, httpOptions);
   }
 }
