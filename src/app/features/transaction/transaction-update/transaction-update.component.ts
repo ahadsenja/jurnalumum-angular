66b1a2323abb2d7e9bc43eabc4e743081d05d1ac
ahadsenja <hamdanilm96@gmail.com>
2021-08-30 06:18:43 +0800

    feat: add onUpdateTransaction function

--- src/app/features/transaction/transaction-update/transaction-update.component.ts
+++ src/app/features/transaction/transaction-update/transaction-update.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Transaction } from 'src/app/shared/models/transaction';
+import { TransactionService } from '../../services/transaction.service';
 
 @Component({
   selector: 'app-transaction-update',
@@ -9,20 +12,55 @@ import { Router } from '@angular/router';
 })
 export class TransactionUpdateComponent implements OnInit {
 
-  formValueGroup = new FormGroup({});
+  formGroupValue = new FormGroup({});
   isSubmitted = false;
 
-  constructor(private router: Router) { }
+  tsObj: Transaction = new Transaction();
+
+  id: number = 0;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private tsService: TransactionService,
+    private formBuilder: FormBuilder
+  ) {
+    this.formGroupValue = this.formBuilder.group({
+      date: [''],
+      description: [''],
+      debit: [0],
+      credit: [0]
+    })
+   }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.tsService.getById(this.id).subscribe(data => {
+      this.tsObj = data
+      this.formGroupValue.patchValue({
+        date: data.date,
+        description: data.description,
+        debit: data.debit,
+        credit: data.credit
+      })
+    })
   }
 
   onUpdateTransaction() {
-    
+    this.tsObj.date = this.formGroupValue.value.date;
+    this.tsObj.description = this.formGroupValue.value.description;
+    this.tsObj.debit = this.formGroupValue.value.debit;
+    this.tsObj.credit = this.formGroupValue.value.credit;
+
+    this.tsService.update(this.tsObj.id, this.tsObj).subscribe(data => {
+      this.isSubmitted = true;
+      this.router.navigate(['/transaction']);
+    }, error => console.log(error))
   }
 
   onCancel() {
-    this.formValueGroup.reset();
+    this.formGroupValue.reset();
     this.router.navigate(['/transaction'])
   }
 
