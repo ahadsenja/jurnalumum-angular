eb82f0175ec7d8c979a34bad65a4c2f64fda788b
ahadsenja <hamdanilm96@gmail.com>
2021-08-30 03:55:34 +0800

    chore: add transaction update routes

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -6,13 +6,17 @@ import { JurnalCreateComponent } from './jurnal/jurnal-create/jurnal-create.comp
 import { JurnalComponent } from './jurnal/jurnal-list/jurnal.component';
 import { TransactionCreateComponent } from './transaction/transaction-create/transaction-create.component';
 import { TransactionComponent } from './transaction/transaction-list/transaction.component';
+import { TransactionUpdateComponent } from './transaction/transaction-update/transaction-update.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
+  
   { path: 'jurnal', component: JurnalComponent },
   { path: 'jurnal/create', component: JurnalCreateComponent },
+
   { path: 'transaction', component: TransactionComponent },
   { path: 'transaction/create', component: TransactionCreateComponent },
+  { path: 'transaction/edit/:id', component: TransactionUpdateComponent },
 ];
 
 @NgModule({
