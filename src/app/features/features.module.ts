20f2c28085f9e6aae8bf83d9abfcb385f0e5eb26
ahadsenja <hamdanilm96@gmail.com>
2021-09-01 01:18:06 +0800

    chore: add ng20Order module

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { Ng2OrderModule } from 'ng2-order-pipe';
 
 import { FeaturesRoutingModule } from './features-routing.module';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+
 import { JurnalComponent } from './jurnal/jurnal-list/jurnal.component';
-import { TransactionComponent } from './transaction/transaction-list/transaction.component';
 import { JurnalCreateComponent } from './jurnal/jurnal-create/jurnal-create.component';
+
+import { TransactionComponent } from './transaction/transaction-list/transaction.component';
 import { TransactionCreateComponent } from './transaction/transaction-create/transaction-create.component';
 import { TransactionUpdateComponent } from './transaction/transaction-update/transaction-update.component';
 
@@ -26,7 +30,8 @@ import { TransactionUpdateComponent } from './transaction/transaction-update/tra
     FormsModule,
     ReactiveFormsModule,
     Ng2SearchPipeModule,
-    NgxPaginationModule
+    NgxPaginationModule,
+    Ng2OrderModule
   ],
   exports: [
     DashboardComponent,
