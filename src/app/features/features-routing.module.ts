f19265ac45159cc4d2e7e75aeb29e3a7bb371d94
ahadsenja <hamdanilm96@gmail.com>
2021-09-13 14:54:02 +0800

    chore: update features routing

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -4,19 +4,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { JurnalCreateComponent } from './jurnal/jurnal-create/jurnal-create.component';
 import { JurnalComponent } from './jurnal/jurnal-list/jurnal.component';
+import { ReportComponent } from './report/report.component';
 import { TransactionCreateComponent } from './transaction/transaction-create/transaction-create.component';
 import { TransactionComponent } from './transaction/transaction-list/transaction.component';
 import { TransactionUpdateComponent } from './transaction/transaction-update/transaction-update.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-  
+
   { path: 'jurnal', component: JurnalComponent },
   { path: 'jurnal/create', component: JurnalCreateComponent },
 
   { path: 'transaction', component: TransactionComponent },
   { path: 'transaction/create', component: TransactionCreateComponent },
   { path: 'transaction/edit/:id', component: TransactionUpdateComponent },
+
+  { path: 'transaction/report', component: ReportComponent }
 ];
 
 @NgModule({
