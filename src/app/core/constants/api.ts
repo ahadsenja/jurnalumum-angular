eb2b5dbbb989a8c0b229cd085c34a0baf8f69306
ahadsenja <hamdanilm96@gmail.com>
2021-09-23 17:25:02 +0800

    refactor: change mockup endpoint with real endpoint from backend

--- src/app/core/constants/api.ts
+++ src/app/core/constants/api.ts
@@ -1,6 +1,6 @@
 export class Api {
- public static LOGIN_URL = 'http://localhost:5000/auth/login';
- public static REGISTER_URL = 'http://localhost:5000/auth/register';
- public static JURNAL_URL = 'http://localhost:5000/jurnals';
- public static TRANSACTION_URL = 'http://localhost:5000/transactions';
+  public static LOGIN_URL = 'http://localhost:8000/api/v1/auth/login';
+  public static REGISTER_URL = 'http://localhost:5000/auth/register';
+  public static JURNAL_URL = 'http://localhost:5000/jurnals';
+  public static TRANSACTION_URL = 'http://localhost:5000/transactions';
 }
